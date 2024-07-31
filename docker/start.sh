483b2109571721c02354d010ba3b614641512329
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2024-07-31 16:56:24 +0100

    implement swoole properly

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# php artisan swoole:http start &
-php artisan serve --host=0.0.0.0 --port=8100 &
-# if [ "${APP_ENV}" = "dev" ]; then
-    php artisan queue:listen
-# else
-    # php artisan queue:work
-# fi
+if [ -e /var/www/.env ]; then
+    source /var/www/.env
+fi
+
+if [ $APP_ENV = 'local' ] || [ $APP_ENV = 'dev' ]; then
+    echo 'running in dev mode - with watch'
+    php artisan octane:start --host=0.0.0.0 --port=8100 --watch &
+else
+    php artisan octane:state --host=0.0.0.0 --port=8100 &
+fi
+
+php artisan queue:listen
