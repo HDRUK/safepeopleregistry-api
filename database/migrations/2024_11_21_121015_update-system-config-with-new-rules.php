29fe5098623c7fb42808cebd00120ba951083c9c
lewissaunders11 <lewis.saunders@hdruk.ac.uk>
2024-11-26 07:14:32 +0000

    update migration file

--- database/migrations/2024_11_21_121015_update-system-config-with-new-rules.php
+++ database/migrations/2024_11_21_121015_update-system-config-with-new-rules.php
@@ -5,38 +5,30 @@ use App\Models\SystemConfig;
 
 return new class extends Migration
 {
-     private const VALIDATION_SCHEMA_JSON = 
-    "{
-        \"validationSchema\": {
-            \"password\": {
-                \"type\": \"string\",
-                \"minLength\": 8,
-                \"maxLength\": 32,
-                \"pattern\": \"^(?=.*\\\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,32}$\"
-            },
-            \"email\": {
-                \"type\": \"string\",
-                \"pattern\": \"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,6}$\"
-            },
-            \"ce_certification_num\": {
-                \"type\": \"string\",
-                \"pattern\": \"^[\\\\w]{4}$\"
-            },
-            \"orc_id\": {
-                \"type\": \"string\",
-                \"pattern\": \"^[\\\\d]{4}-[\\\\d]{4}-[\\\\d]{4}-[\\\\d]{3}(0|9|X)$\"
-            },
-            \"company_number\": {
-                \"type\": \"string\",
-                \"pattern\": \"^((AC|ZC|FC|GE|LP|OC|SE|SA|SZ|SF|GS|SL|SO|SC|ES|NA|NZ|NF|GN|NL|NC|R0|NI|EN|\\\\d{2}|SG|FE)\\\\d{5}(\\\\d|C|R))|((RS|SO)\\\\d{3}(\\\\d{3}|\\\\d{2}[WSRCZF]|\\\\d(FI|RS|SA|IP|US|EN|AS)|CUS))|((NI|SL)\\\\d{5}[\\\\dA])|(OC(([\\\\dP]{5}[CWERTB])|([\\\\dP]{4}(OC|CU))))$\"
-            },
-            \"postcode\": {
-                \"type\": \"string\",
-                \"maxLength\": 8,
-                \"pattern\": \"[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? [0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}\"
-            }
-        }
-    }";
+    private const validationSchema = [
+            "ce_certification_num" => [
+                "type" => "string",
+                "pattern" => "[a-zA-Z0-9]+-[a-zA-Z0-9]+-[a-zA-Z0-9]+-[a-zA-Z0-9]+-[a-zA-Z0-9]+",
+            ],
+            "orc_id" => [
+                "type" => "string",
+                "pattern" => "^[\\d]{4}-[\\d]{4}-[\\d]{4}-[\\d]{3}(0|9|X)$",
+            ],
+            "company_number" => [
+                "type" => "string",
+                "pattern" => "^((AC|ZC|FC|GE|LP|OC|SE|SA|SZ|SF|GS|SL|SO|SC|ES|NA|NZ|NF|GN|NL|NC|R0|NI|EN|\\d{2}|SG|FE)\\d{5}(\\d|C|R))|((RS|SO)\\d{3}(\\d{3}|\\d{2}[WSRCZF]|\\d(FI|RS|SA|IP|US|EN|AS)|CUS))|((NI|SL)\\d{5}[\\dA])|(OC(([\\dP]{5}[CWERTB])|([\\dP]{4}(OC|CU))))$",
+            ],
+            "postcode" => [
+                "type" => "string",
+                "maxLength" => 8,
+                "pattern" => "[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? [0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}",
+            ],
+            "otp" => [
+                "type" => "string",
+                "minLength" => 6,
+                "maxLength" => 6,
+            ]
+        ];
 
     /**
      * Run the migrations.
@@ -46,9 +38,10 @@ return new class extends Migration
         $config = SystemConfig::where('name', 'VALIDATION_SCHEMA')->first();
 
         if ($config) {
-            $newValue = json_decode(self::VALIDATION_SCHEMA_JSON, true);
+            $oldValue = json_decode($config->value, true)['validationSchema'];
 
-            $config->value = json_encode($newValue);
+            $updatedSchema = array_merge($oldValue, self::validationSchema);
+            $config->value = ['validationSchema' => $updatedSchema];        
             $config->save();
         }
     }
@@ -61,7 +54,7 @@ return new class extends Migration
         $config = SystemConfig::where('name', 'VALIDATION_SCHEMA')->first();
 
         if ($config) {
-            $schema = json_decode(self::VALIDATION_SCHEMA_JSON, true);
+            $schema = json_decode($config->value, true);
 
             $validationSchema = $schema['validationSchema'] ?? [];
             $filteredSchema = array_intersect_key($validationSchema, array_flip(['password', 'email']));
