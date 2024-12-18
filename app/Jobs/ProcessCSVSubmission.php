08a597523647b8b22550a404b66eb0642e874703
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-12-18 15:20:13 +0000

    chore: unneeded import

--- app/Jobs/ProcessCSVSubmission.php
+++ app/Jobs/ProcessCSVSubmission.php
@@ -12,7 +12,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use UserUtils;
 use TriggerEmail;
 use RegistryManagementController as RMC;
 
