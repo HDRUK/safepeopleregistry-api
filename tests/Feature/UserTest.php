42936a507d13a0a00754651a10cef88abdd19705
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2024-12-20 17:05:16 +0000

    further changes to fix tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use Carbon\Carbon;
+use RegistryManagementController as RMC;
 use KeycloakGuard\ActingAsKeycloakUser;
 use App\Models\User;
+use App\Models\Identity;
 use Database\Seeders\EmailTemplatesSeeder;
 use Database\Seeders\CustodianSeeder;
 use Database\Seeders\PermissionSeeder;
@@ -228,8 +231,52 @@ class UserTest extends TestCase
     {
         $response = $this->actingAsKeycloakUser($this->user, $this->getMockedKeycloakPayload())
         ->json(
+            'POST',
+            self::TEST_URL,
+            [
+            'first_name' => fake()->firstname(),
+            'last_name' => fake()->lastname(),
+            'email' => fake()->email(),
+            'provider' => fake()->word(),
+            'provider_sub' => Str::random(10),
+            'public_opt_in' => fake()->randomElement([0, 1]),
+            'declaration_signed' => fake()->randomElement([0, 1]),
+        ]
+        );
+
+        $response->assertStatus(201);
+        $this->assertArrayHasKey('data', $response);
+
+        $content = $response->decodeResponseJson()['data'];
+
+        $registry = RMC::createRegistryLedger();
+        $user = User::where('id', $content)->first();
+        $user->update([
+            'registry_id' => $registry->id,
+        ]);
+
+        $identity = Identity::create([
+            'registry_id' =>            $registry->id,
+            'selfie_path' =>            '',
+            'passport_path' =>          '',
+            'drivers_license_path' =>   '',
+            'address_1' =>              '123 Fake Street',
+            'address_2' =>              '',
+            'town' =>                   'Fake Town',
+            'county' =>                 'Fake County',
+            'country' =>                'UK',
+            'postcode' =>               'T35 T3D',
+            'dob' =>                    '1977-07-25',
+            'idvt_result' =>            1,
+            'idvt_result_perc' =>       100,
+            'idvt_errors' =>            null,
+            'idvt_completed_at' =>      Carbon::now(),
+        ]);
+
+        $response = $this->actingAsKeycloakUser($this->user, $this->getMockedKeycloakPayload())
+        ->json(
             'GET',
-            self::TEST_URL . '/10' // One of the researchers
+            self::TEST_URL . '/' . $user->id // One of the researchers
         );
 
         $response->assertStatus(200);
