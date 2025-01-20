7e01f4f8cd56716575142660df32010c5c8a3098
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2025-01-20 15:03:15 +0000

    remove commented code

--- app/Keycloak/Keycloak.php
+++ app/Keycloak/Keycloak.php
@@ -271,14 +271,6 @@ class Keycloak
         try {
             $authUrl = env('KEYCLOAK_BASE_URL').'/realms/'.env('KEYCLOAK_REALM').'/protocol/openid-connect/token';
 
-            // $credentials = [
-            //     'grant_type' => 'password',
-            //     'client_id' => env('KEYCLOAK_CLIENT_ID'),
-            //     'client_secret' => env('KEYCLOAK_CLIENT_SECRET'),
-            //     'username' => 'developers@hdruk.ac.uk',
-            //     'password' => 'Challenge12Havoc!',
-            // ];
-
             $credentials = [
                 'client_secret' => env('KEYCLOAK_CLIENT_SECRET'),
                 'client_id' => env('KEYCLOAK_CLIENT_ID'),
