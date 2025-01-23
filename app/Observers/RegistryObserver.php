690da029b50ba7eab4aea1ad36d875bfa5877282
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2025-01-23 11:55:13 +0000

    again, trying to fix pipeline using local code

--- app/Observers/RegistryObserver.php
+++ app/Observers/RegistryObserver.php
@@ -11,7 +11,7 @@ class RegistryObserver
     public function created(Registry $registry)
     {
         $user = User::where('registry_id', $registry->id)->first();
-        if ($user !== null) {
+        if ($user !== null || !in_array(env('APP_ENV'), ['testing', 'ci'])) {
             Keycloak::updateSoursdDigitalIdentifier($user);
         }
     }
