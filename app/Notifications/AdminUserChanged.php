7e61c25927574eb247404557760a9fedd66cbb99
Calum Macdonald <calmacx@gmail.com>
2025-01-24 16:10:11 +0000

    forgot to add notifications

---
+++ app/Notifications/AdminUserChanged.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class AdminUserChanged extends Notification
+{
+    use Queueable;
+
+    private $message;
+    private $details;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($user, $changes)
+    {
+        $this->message = "$user->first_name $user->last_name details changed!";
+        $this->details = $changes;
+    }
+
+    /**
+     * Specify the delivery channels.
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Store the notification in the database.
+     */
+    public function toDatabase($notifiable)
+    {
+        return [
+            'message' => $this->message,
+            'details' => $this->details,
+            'time' => now(),
+        ];
+    }
+}
