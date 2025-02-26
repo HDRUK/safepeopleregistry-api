de1e2c84d53e40d224cfe1d382710bcf2eb2d3d5
Calum Macdonald <calmacx@gmail.com>
2025-02-26 13:20:45 +0000

    fix for horizon

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -27,7 +27,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      */
     protected function gate(): void
     {
-        Gate::define('viewHorizon', function (User $user = null) {
+        Gate::define('viewHorizon', function () {
             return true;
         });
     }
