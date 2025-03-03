fd0c87c6704692004b865e8b29e5a8b8a3817bef
Calum Macdonald <calmacx@gmail.com>
2025-03-03 16:42:09 +0000

    reverting files

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Jobs\UpdateActionNotifications;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,10 +13,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call(function () {
-            UpdateActionNotifications::dispatch();
-        })->everyMinute();
-
     }
 
     /**
