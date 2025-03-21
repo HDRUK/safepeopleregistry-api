b4c33b713f39ddc60fefbecd484a3a28547af6e9
Calum Macdonald <calmacx@gmail.com>
2025-03-21 15:47:09 +0000

    phpstan error

--- app/Models/RegistryHasAffiliation.php
+++ app/Models/RegistryHasAffiliation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RegistryHasAffiliation extends Model
 {
@@ -18,7 +19,7 @@ class RegistryHasAffiliation extends Model
         'affiliation_id',
     ];
 
-    public function affiliation()
+    public function affiliation(): BelongsTo
     {
         return $this->belongsTo(Affiliation::class, 'affiliation_id');
     }
