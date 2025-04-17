e106a130dcc5ae1feecbc1f18cc0f5f866040639
peter-hammans <164904644+peter-hammans@users.noreply.github.com>
2025-04-17 10:12:17 +0100

    feat(SOURSD-994): custodian user by custodian (#279)

--- app/Http/Controllers/Api/V1/CustodianController.php
+++ app/Http/Controllers/Api/V1/CustodianController.php
@@ -15,6 +15,7 @@ use App\Models\CustodianHasRule;
 use App\Models\Rules;
 use App\Models\Project;
 use App\Models\Registry;
+use App\Models\CustodianUser;
 use App\Traits\CommonFunctions;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -956,6 +957,54 @@ class CustodianController extends Controller
             'message' => 'success',
             'data' => $custodian->rules
         ]);
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/api/v1/custodians/{custodianId}/custodian_users",
+     *      summary="Get list of people for a custodian",
+     *      description="Fetches the list of custodian users based on the custodian id.",
+     *      tags={"Custodians"},
+     *      @OA\Parameter(
+     *          name="custodianId",
+     *          in="path",
+     *          required=true,
+     *          description="ID of the custodian",
+     *          @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successfully retrieved custodian users",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="success"),
+     *              @OA\Property(property="data", type="array",
+     *                  @OA\Items(
+     *                      ref="#/components/schemas/CustodianUser"
+     *                  )
+     *              )
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Custodian users not found",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(property="message", type="string", example="Custodian users not found")
+     *          )
+     *      )
+     * )
+     */
+    public function getCustodianUsers(Request $request, int $custodianId): JsonResponse
+    {
+        $users = CustodianUser::searchViaRequest()->where('custodian_id', $custodianId)
+            ->applySorting()->with("userPermissions.permission")
+            ->paginate((int)$this->getSystemConfig('PER_PAGE'));
+
+        if ($users) {
+            return $this->OKResponse($users);
+        }
+
+        return $this->NotFoundResponse();
     }
 
     /**
--- app/Models/CustodianUser.php
+++ app/Models/CustodianUser.php
@@ -4,10 +4,49 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\SearchManager;
 
+/**
+ * @OA\Schema(
+ *      schema="CustodianUser",
+ *      title="Custodian User",
+ *      description="CustodianUser model",
+ *      @OA\Property(property="id",
+ *          type="integer",
+ *          example=1,
+ *          description="Model primary key"
+ *      ),
+ *      @OA\Property(property="custodian_id",
+ *          type="integer",
+ *          example=1,
+ *          description="Custodian primary key"
+ *      ),
+ *      @OA\Property(property="created_at",
+ *          type="string",
+ *          example="2023-10-10T15:03:00Z"
+ *      ),
+ *      @OA\Property(property="updated_at",
+ *          type="string",
+ *          example="2023-10-10T15:03:00Z"
+ *      ),
+ *      @OA\Property(property="first_name",
+ *          type="string",
+ *          example="John"
+ *      ),
+ *      @OA\Property(property="last_name",
+ *          type="string",
+ *          example="Smith"
+ *      ),
+ *      @OA\Property(property="email",
+ *          type="string",
+ *          example="First name"
+ *      )
+ * )
+ */
 class CustodianUser extends Model
 {
     use HasFactory;
+    use SearchManager;
 
     public $table = 'custodian_users';
 
@@ -26,6 +65,18 @@ class CustodianUser extends Model
     protected $hidden = [
         'password',
         'keycloak_id',
+    ];
+
+    protected static array $searchableColumns = [
+        'first_name',
+        'last_name',
+        'email'
+    ];
+
+    protected static array $sortableColumns = [
+        'first_name',
+        'last_name',
+        'email'
     ];
 
     /**
