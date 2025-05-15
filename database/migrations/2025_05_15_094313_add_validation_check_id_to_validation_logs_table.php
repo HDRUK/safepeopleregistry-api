9d4c9b33e3fff9ac6297af7a9a9eeb920b90887d
Calum Macdonald <calmacx@gmail.com>
2025-05-15 10:44:31 +0100

    add updates to validation log stuff

---
+++ database/migrations/2025_05_15_094313_add_validation_check_id_to_validation_logs_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('validation_logs', function (Blueprint $table) {
+            $table->unsignedBigInteger('validation_check_id')->nullable()->after('entity_type');
+            $table->foreign('validation_check_id')
+                ->references('id')
+                ->on('validation_checks')
+                ->onDelete('set null');
+            $table->dropColumn('name');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('validation_logs', function (Blueprint $table) {
+            $table->dropForeign(['validation_check_id']);
+            $table->dropColumn('validation_check_id');
+            $table->string('name')->nullable();
+        });
+    }
+};
