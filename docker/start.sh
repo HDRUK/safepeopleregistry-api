353981c1e07d06bd7447c02880e15f3ef2c3587a
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2025-06-09 13:45:35 +0100

    fix(SOURSD-880): replaces frankenphp with roadrunner for finer tuning of resource limits

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,7 @@ if [ -e /var/www/.env ]; then
     source /var/www/.env
 fi
 
-base_command="php artisan octane:frankenphp --max-requests=250 --host=0.0.0.0 --port=8100"
+base_command="php artisan octane:start --server=roadrunner --max-requests=250 --host=0.0.0.0 --port=8100"
 
 if [ $APP_ENV = 'local' ] || [ $APP_ENV = 'dev' ]; then
     echo 'running in dev mode - with watch'
@@ -25,11 +25,6 @@ else
     php artisan validation:generate-logs
 
     echo "running in prod mode"
-fi
-
-# Add workers option if OCTANE_WORKERS is set
-if [ -n "$OCTANE_WORKERS" ]; then
-    base_command="$base_command --workers=${OCTANE_WORKERS}"
 fi
 
 $base_command &
