f391a5b1c3d5ce7121de2c536b4f95e3f00104a2
Calum Macdonald <calmacx@gmail.com>
2025-06-11 12:48:16 +0100

    missing files

---
+++ app/Models/CustodianHasProjectUser.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Traits\StateWorkflow;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+/**
+ *
+ * @property int $project_has_user_id
+ * @property int $custodian_id
+ * @property int $approved
+ * @property string|null $comment
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property-read \App\Models\Custodian $custodian
+ * @property-read \App\Models\ProjectHasUser $projectHasUser
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CustodianHasProjectUser newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CustodianHasProjectUser newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CustodianHasProjectUser query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CustodianHasProjectUser whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CustodianHasProjectUser whereCustodianId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|CustodianHasProjectUser whereProjectUserId($value)
+ * @mixin \Eloquent
+ *
+ * @OA\Schema(
+ *     schema="CustodianHasProjectUser",
+ *     title="Custodian Has Project User",
+ *     @OA\Property(property="id", type="integer", example=1),
+ *     @OA\Property(property="project_has_user_id", type="integer", example=1),
+ *     @OA\Property(property="custodian_id", type="integer", example=1),
+ *     @OA\Property(property="created_at", type="string", format="date-time"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time"),
+ *     @OA\Property(property="projectHasUser", ref="#/components/schemas/ProjectHasUser"),
+ * )
+ */
+
+class CustodianHasProjectUser extends Model
+{
+    use StateWorkflow;
+    protected $table = 'custodian_has_project_has_user';
+
+    public $timestamps = true;
+
+    protected $casts = [
+        'created_at' => 'datetime',
+    ];
+
+    protected $fillable = [
+        'project_has_user_id',
+        'custodian_id',
+    ];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($model) {
+            if (in_array(StateWorkflow::class, class_uses($model))) {
+                $model->setState(State::STATE_FORM_RECEIVED);
+                $model->save();
+            }
+        });
+    }
+
+    /**
+     *  @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\ProjectHasUser>
+     */
+    public function projectHasUser(): BelongsTo
+    {
+        return $this->belongsTo(ProjectHasUser::class, 'project_has_user_id');
+    }
+
+    /**
+     *  @return \Illuminate\Database\Eloquent\Relations\BelongsTo<\App\Models\Custodian>
+     */
+    public function custodian(): BelongsTo
+    {
+        return $this->belongsTo(Custodian::class, 'custodian_id');
+    }
+
+    public function modelState(): MorphOne
+    {
+        return $this->morphOne(ModelState::class, 'stateable');
+    }
+}
