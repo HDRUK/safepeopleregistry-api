053a53ac843399c4f14cf1c62a51eda9d9158c6a
Peter Hammans <peter.hammans@hdruk.ac.uk>
2025-07-02 15:54:26 +0100

    fix: migration null

---
+++ database/migrations/2025_07_01_152257_update-project-details-access-type-nullable.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('project_details', function (Blueprint $table) {
+            $table->text('access_type')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('project_details', function (Blueprint $table) {
+            $table->tinyInteger('access_type')->nullable(false)->change();
+        });
+    }
+};
