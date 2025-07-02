fefaf01819713ccfdce43ef3d967f2c9c993b4ec
Dan Nita <dan.nita@hdruk.ac.uk>
2025-07-02 11:06:27 +0100

    update

--- public/frankenphp-worker.php
+++ public/frankenphp-worker.php
@@ -8,4 +8,6 @@ error_reporting(E_ALL);
 $_SERVER['APP_BASE_PATH'] = $_ENV['APP_BASE_PATH'] ?? $_SERVER['APP_BASE_PATH'] ?? __DIR__.'/..';
 $_SERVER['APP_PUBLIC_PATH'] = $_ENV['APP_PUBLIC_PATH'] ?? $_SERVER['APP_BASE_PATH'] ?? __DIR__;
 
+echo "Reached worker\n";
+
 require __DIR__.'/../vendor/laravel/octane/bin/frankenphp-worker.php';
