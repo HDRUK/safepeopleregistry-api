2dd9de16c33f6793e4b9924342697c51df4c1e19
Dan Nita <dan.nita@hdruk.ac.uk>
2025-07-04 13:38:05 +0100

    update

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,7 @@ if [ -e /var/www/.env ]; then
     source /var/www/.env
 fi
 
-base_command="php artisan octane:start --host=0.0.0.0 --port=8000"
+base_command="php artisan octane:start --host=0.0.0.0 --port=8100"
 # base_command="php artisan octane:start --server=swoole --host=0.0.0.0 --port=8100"
 # base_command="./rr serve --config=.rr.yaml"
 # base_command="php artisan serve --host=0.0.0.0 --port=8100"
