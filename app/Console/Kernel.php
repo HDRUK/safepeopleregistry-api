025c53727244bff574977313a6ccf2633af9e95e
Dan Nita <dan.nita@hdruk.ac.uk>
2025-07-07 14:40:05 +0100

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,9 +14,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->call(function () {
-        //     UpdateActionNotifications::dispatch();
-        // })->everyMinute();
+        $schedule->call(function () {
+            UpdateActionNotifications::dispatch();
+        })->everyMinute();
 
     }
 
