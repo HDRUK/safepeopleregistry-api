81f210fb161b7a8d11962433a7eb583348d44dfc
Dan Nita <dan.nita@hdruk.ac.uk>
2025-07-07 19:24:59 +0100

    update

--- docker/scheduler.sh
+++ docker/scheduler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-    php /var/www/artisan schedule:run --verbose --no-interaction >> /dev/stdout 2>> /dev/stderr
+    php /var/www/artisan schedule:run --verbose --no-interaction
     sleep 60
 done
