0fd29b4a22d657478b59762a33ba5a7b9f5de1f8
Dan Nita <dan.nita@hdruk.ac.uk>
2025-07-07 11:57:04 +0100

    update

--- docker/start.sh
+++ docker/start.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
 
-if [ -e /var/www/.env ]; then
-    source /var/www/.env
-fi
-
-# base_command="php artisan octane:start --server=swoole --host=0.0.0.0 --port=8100 --workers=4 --max-requests=100"
-# base_command="php artisan octane:start --server=swoole --host=0.0.0.0 --port=8100"
-# base_command="./rr serve --config=.rr.yaml"
-base_command="php artisan serve --host=0.0.0.0 --port=8100"
-
-if [ $APP_ENV = 'local' ] || [ $APP_ENV = 'dev' ]; then
-    echo 'running in dev mode - with watch'
-    # base_command="$base_command --watch"
-
-    if [ $REBUILD_DB = 1 ]; then
-        # Completely clear down the data in local/dev envs
-        php artisan migrate:fresh
-        php artisan db:seed --class=BaseDemoSeeder
-    else
-        php artisan migrate
-    fi
-else
-    # Only forward-facing migrations anywhere else
-    php artisan migrate
-    # call the email template seeder to updateOrCreate without truncating first
-    DISABLE_TRUNCATE=true php artisan db:seed --class=EmailTemplatesSeeder
-    php artisan validation:generate-logs
-
-    echo "running in prod mode"
-fi
-
-# if [ -n "$OCTANE_WORKERS" ]; then
-#     base_command="$base_command --workers=${OCTANE_WORKERS}"
+# if [ -e /var/www/.env ]; then
+#     source /var/www/.env
 # fi
 
+# # base_command="php artisan octane:start --server=swoole --host=0.0.0.0 --port=8100 --workers=4 --max-requests=100"
+# # base_command="php artisan octane:start --server=swoole --host=0.0.0.0 --port=8100"
+# # base_command="./rr serve --config=.rr.yaml"
+# base_command="php artisan serve --host=0.0.0.0 --port=8100"
+
+# if [ $APP_ENV = 'local' ] || [ $APP_ENV = 'dev' ]; then
+#     echo 'running in dev mode - with watch'
+#     # base_command="$base_command --watch"
+
+#     if [ $REBUILD_DB = 1 ]; then
+#         # Completely clear down the data in local/dev envs
+#         php artisan migrate:fresh
+#         php artisan db:seed --class=BaseDemoSeeder
+#     else
+#         php artisan migrate
+#     fi
+# else
+#     # Only forward-facing migrations anywhere else
+#     php artisan migrate
+#     # call the email template seeder to updateOrCreate without truncating first
+#     DISABLE_TRUNCATE=true php artisan db:seed --class=EmailTemplatesSeeder
+#     php artisan validation:generate-logs
+
+#     echo "running in prod mode"
+# fi
+
+# # if [ -n "$OCTANE_WORKERS" ]; then
+# #     base_command="$base_command --workers=${OCTANE_WORKERS}"
+# # fi
+
+# php artisan horizon &
+# $base_command
+
+# Run Laravel Horizon in background
 php artisan horizon &
-$base_command
+
+# Start PHP-FPM in background
+php-fpm -D
+
+# Start Nginx in foreground
+nginx -g "daemon off;"
