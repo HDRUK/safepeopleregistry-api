5845eaa675f7adbb36ceeb8d9580ead5f2ea9cf4
Dan Nita <dan.nita@hdruk.ac.uk>
2025-07-09 10:35:50 +0100

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
         // $schedule->call(function () {
         //     UpdateActionNotifications::dispatch();
         // })->everyMinute();
-        $schedule->job(new UpdateActionNotifications)->everyMinute();
+        // $schedule->job(new UpdateActionNotifications)->everyMinute();
 
     }
 
