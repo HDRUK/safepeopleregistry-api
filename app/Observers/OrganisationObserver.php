9beaca55f69c067158964e676763f96f34b97125
Calum Macdonald <calmacx@gmail.com>
2025-07-09 08:26:42 +0100

    update

--- app/Observers/OrganisationObserver.php
+++ app/Observers/OrganisationObserver.php
@@ -61,6 +61,13 @@ class OrganisationObserver
                 'completed_at' => null,
             ]);
         }
+
+        // Force completeness checks on creation
+        $this->checkIsComplete($organisation, $this->nameAndAddressFields, Organisation::ACTION_NAME_ADDRESS_COMPLETED, true);
+        $this->checkIsComplete($organisation, $this->digitalIdentifiers, Organisation::ACTION_DIGITAL_ID_COMPLETED, true);
+        $this->checkIsComplete($organisation, $this->sectorSize, Organisation::ACTION_SECTOR_SIZE_COMPLETED, true);
+        $this->checkIsComplete($organisation, $this->securityCompliance, Organisation::ACTION_DATA_SECURITY_COMPLETED, true);
+
         $this->manageAffiliationStates($organisation);
 
         $custodianIds = Custodian::select("id")->pluck("id");
@@ -77,68 +84,22 @@ class OrganisationObserver
      */
     public function updated(Organisation $organisation): void
     {
-        $this->checkIsComplete(
-            $organisation,
-            $this->nameAndAddressFields,
-            Organisation::ACTION_NAME_ADDRESS_COMPLETED
-        );
-
-        $this->checkIsComplete(
-            $organisation,
-            $this->digitalIdentifiers,
-            Organisation::ACTION_DIGITAL_ID_COMPLETED
-        );
-
-        $this->checkIsComplete(
-            $organisation,
-            $this->sectorSize,
-            Organisation::ACTION_SECTOR_SIZE_COMPLETED
-        );
-
-        // note - future improvement to also check the date is not expired
-        $this->checkIsComplete(
-            $organisation,
-            $this->securityCompliance,
-            Organisation::ACTION_DATA_SECURITY_COMPLETED
-        );
-
+        $this->checkIsComplete($organisation, $this->nameAndAddressFields, Organisation::ACTION_NAME_ADDRESS_COMPLETED);
+        $this->checkIsComplete($organisation, $this->digitalIdentifiers, Organisation::ACTION_DIGITAL_ID_COMPLETED);
+        $this->checkIsComplete($organisation, $this->sectorSize, Organisation::ACTION_SECTOR_SIZE_COMPLETED);
+        $this->checkIsComplete($organisation, $this->securityCompliance, Organisation::ACTION_DATA_SECURITY_COMPLETED);
         $this->manageAffiliationStates($organisation);
     }
 
-    /**
-     * Handle the Organisation "deleted" event.
-     */
-    public function deleted(Organisation $organisation): void
+    private function checkIsComplete(Organisation $organisation, array $fields, string $action, bool $force = false): void
     {
-        //
-    }
-
-    /**
-     * Handle the Organisation "restored" event.
-     */
-    public function restored(Organisation $organisation): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Organisation "force deleted" event.
-     */
-    public function forceDeleted(Organisation $organisation): void
-    {
-        //
-    }
-
-    private function checkIsComplete(Organisation $organisation, array $fields, string $action): void
-    {
-        if ($organisation->isDirty($fields)) {
-            $isProfileComplete = collect($fields)
-                ->every(function ($field) use ($organisation) {
-                    if ($this->isDateField($field)) {
-                        return $this->isDateValid($organisation->$field);
-                    }
-                    return !empty($organisation->$field);
-                });
+        if ($force || $organisation->isDirty($fields)) {
+            $isProfileComplete = collect($fields)->every(function ($field) use ($organisation) {
+                if ($this->isDateField($field)) {
+                    return $this->isDateValid($organisation->$field);
+                }
+                return !empty($organisation->$field);
+            });
 
             ActionLog::updateOrCreate(
                 [
@@ -151,13 +112,12 @@ class OrganisationObserver
         }
     }
 
-    private function manageAffiliationStates(Organisation $organisation)
+    private function manageAffiliationStates(Organisation $organisation): void
     {
         if ($organisation->isDirty('unclaimed')) {
             $unclaimed = $organisation->unclaimed;
             $state = $unclaimed ? State::STATE_AFFILIATION_INVITED : State::STATE_AFFILIATION_PENDING;
-            $affiliations = Affiliation::where("organisation_id", $organisation->id)
-                ->get();
+            $affiliations = Affiliation::where("organisation_id", $organisation->id)->get();
 
             foreach ($affiliations as $affiliation) {
                 $affiliation->setState($state);
@@ -165,18 +125,11 @@ class OrganisationObserver
         }
     }
 
-
-    /**
-     * Helper function to check if a field is an expiry date.
-     */
     private function isDateField(string $field): bool
     {
         return str_contains($field, '_expiry_date');
     }
 
-    /**
-     * Helper function to check if a date is valid (not expired).
-     */
     private function isDateValid(?string $date): bool
     {
         if (!$date) {
