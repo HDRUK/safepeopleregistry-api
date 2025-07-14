c4803ae2eba30ce5e98754e3e493ed49a1d3c2d9
Calum Macdonald <calmacx@gmail.com>
2025-07-14 09:56:13 +0100

    fix

--- app/Http/Controllers/Api/V1/OrganisationController.php
+++ app/Http/Controllers/Api/V1/OrganisationController.php
@@ -1318,8 +1318,6 @@ class OrganisationController extends Controller
                 ->with([
                     'registry.affiliations' => function ($query) use ($id) {
                         $query->where('organisation_id', $id)->limit(1);
-                        $query->filterByState()
-                            ->with("modelState.state");
                     },
                     'registry.affiliations.modelState.state',
                     'modelState.state'
