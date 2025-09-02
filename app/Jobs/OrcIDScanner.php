f8a212a1aeeb770cbad4cbf2bd1f354a8fd6057c
Peter Hammans <peter.hammans@hdruk.ac.uk>
2025-09-02 11:05:45 +0100

    fix: disable orcid scan in test

--- app/Jobs/OrcIDScanner.php
+++ app/Jobs/OrcIDScanner.php
@@ -44,7 +44,7 @@ class OrcIDScanner implements ShouldQueue
     public function handle(): void
     {
         try {
-            if ($this->user->consent_scrape && $this->user->orc_id !== null && $this->user->user_group === User::GROUP_USERS) {
+            if (env('ORCID_ENABLED', true) && $this->user->consent_scrape && $this->user->orc_id !== null && $this->user->user_group === User::GROUP_USERS) {
                 $this->user->orcid_scanning = 1;
                 $this->user->save();
 
