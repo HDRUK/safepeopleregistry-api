ef4b423a334a06cd3df6bb18f8875ac0c9d629dd
Dan Nita <dan.nita@hdruk.ac.uk>
2025-09-15 15:29:12 +0100

    clear cache preprod

--- docker/start.sh
+++ docker/start.sh
@@ -20,6 +20,7 @@ if [ $APP_ENV = 'local' ] || [ $APP_ENV = 'dev' ]; then
         php artisan migrate
     fi
 else
+    php artisan cache:clear
     # Only forward-facing migrations anywhere else
     php artisan migrate
     # call the email template seeder to updateOrCreate without truncating first
