e4268ee533d33c72098d87c7bcc6516830da355f
Dan Nita <dan.nita@hdruk.ac.uk>
2025-09-15 14:00:18 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// stop all all other routes
+Route::any('{path}', function () {
+    $response = [
+        'message' => 'Resource not found',
+    ];
+
+    return response()->json($response)
+        ->setStatusCode(404);
 });
