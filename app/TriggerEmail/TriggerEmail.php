0825fbd6648060ff3ff41b1e5ec2b21715282ac7
Dan Nita <dan.nita@hdruk.ac.uk>
2025-09-16 15:47:44 +0100

    update

--- app/TriggerEmail/TriggerEmail.php
+++ app/TriggerEmail/TriggerEmail.php
@@ -219,6 +219,7 @@ class TriggerEmail
                     '[[env(SUPPORT_EMAIL)]]' => config('speedi.system.support_email'),
                     '[[env(REGISTRY_IMAGE_URL)]]' => config('speedi.system.registry_image_url'),
                     '[[env(PORTAL_URL)]]' => config('speedi.system.portal_url'),
+                    '[[env(PORTAL_PATH_INVITE)]]' => config('speedi.system.portal_path_invite'),
                 ];
 
                 PendingInvite::create([
