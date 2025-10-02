63ea618d1b764f0a6ec5cf2f0fc94039710016b3
Dan Nita <dan.nita@hdruk.ac.uk>
2025-10-02 09:19:07 +0100

    update

--- app/Http/Controllers/Api/V1/AuditLogController.php
+++ app/Http/Controllers/Api/V1/AuditLogController.php
@@ -83,7 +83,7 @@ class AuditLogController extends Controller
                                 ->select(['id','registry_id'])
                                 ->with([
                                     'registry:id',
-                                    'registry.user:id,first_name,last_name,registry_id,evaluation',
+                                    'registry.user:id,first_name,last_name,registry_id',
                                 ]),
                         ])->morphWith([
                             Affiliation::class => ['registry.user'],
