e2a58d6d53b86a7178b7d815c3ea351cb06bffd1
peter-hammans <164904644+peter-hammans@users.noreply.github.com>
2025-10-03 10:13:21 +0100

    feat(REGISTRY-1920): change notifications (#466)

--- app/Notifications/ProjectHasUser/ProjectHasUserCreatedEntityUser.php
+++ app/Notifications/ProjectHasUser/ProjectHasUserCreatedEntityUser.php
@@ -4,7 +4,6 @@ namespace App\Notifications\ProjectHasUser;
 
 use App\Models\Affiliation;
 use App\Models\User;
-use App\Models\Custodian;
 use App\Models\Organisation;
 use App\Models\Project;
 use Illuminate\Bus\Queueable;
@@ -16,14 +15,13 @@ class ProjectHasUserCreatedEntityUser extends Notification
     use Queueable;
     use ProjectHasUserNotification;
 
-    public function __construct(Custodian $custodian, Project $project, Organisation $organisation, Affiliation $affiliation, User $user)
+    public function __construct(Project $project, Organisation $organisation, Affiliation $affiliation, User $user)
     {
-        $message = $custodian->name . " added you and Organisation " . $organisation->organisation_name . " to " . $project->title;
+        $message = "A custodian added you and Organisation " . $organisation->organisation_name . " to " . $project->title;
 
         $this->buildNotification($message, $affiliation);
 
-        $this->sendEmail($affiliation, $user, [
-            '[[custodian.name]]' => $custodian->name,
+        $this->sendEmail($affiliation, $affiliation->email, $user->id, [
             '[[user.email]]' => $affiliation->email,
             '[[organisation.name]]' => $organisation->organisation_name,
             '[[project.title]]' => $project->title,
