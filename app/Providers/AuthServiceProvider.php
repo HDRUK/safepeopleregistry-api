607c6bb4acc9a4c85075ee09fd71d593a82ee489
Dan Nita <dan.nita@hdruk.ac.uk>
2025-10-14 14:22:34 +0100

    Consistently setup Gates/Policies for endpoint permissions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,17 +2,34 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use App\Models\User;
-use App\Models\Registry;
-use App\Models\Custodian;
-use App\Models\Organisation;
 use App\Models\Project;
+use App\Models\Identity;
+use App\Models\Registry;
+use App\Models\ActionLog;
+use App\Models\Custodian;
+use App\Models\Department;
+use App\Models\Experience;
+use App\Models\Affiliation;
+use App\Models\Endorsement;
+use App\Models\Organisation;
 use App\Policies\UserPolicy;
-use App\Policies\RegistryPolicy;
-use App\Policies\CustodianPolicy;
-use App\Policies\OrganisationPolicy;
+use App\Models\CustodianUser;
+use App\Models\ValidationLog;
+use App\Models\ValidationLogComment;
 use App\Policies\ProjectPolicy;
+use App\Policies\IdentityPolicy;
+use App\Policies\RegistryPolicy;
+use App\Policies\ActionLogPolicy;
+use App\Policies\CustodianPolicy;
+use App\Policies\DepartmentPolicy;
+use App\Policies\ExperiencePolicy;
+use App\Policies\AffiliationPolicy;
+use App\Policies\EndorsementPolicy;
+use App\Policies\OrganisationPolicy;
+use Illuminate\Support\Facades\Gate;
+use App\Policies\CustodianUserPolicy;
+use App\Policies\ValidationLogPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -23,11 +40,20 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        User::class => UserPolicy::class,
-        Registry::class => RegistryPolicy::class,
+        ActionLog::class => ActionLogPolicy::class,
+        Affiliation::class => AffiliationPolicy::class,
         Custodian::class => CustodianPolicy::class,
+        CustodianUser::class => CustodianUserPolicy::class,
+        Department::class => DepartmentPolicy::class,
+        Endorsement::class => EndorsementPolicy::class,
+        Experience::class => ExperiencePolicy::class,
+        Identity::class => IdentityPolicy::class,
         Organisation::class => OrganisationPolicy::class,
         Project::class => ProjectPolicy::class,
+        Registry::class => RegistryPolicy::class,
+        User::class => UserPolicy::class,
+        ValidationLogComment::class => ValidationLogPolicy::class,
+        ValidationLog::class => ValidationLogPolicy::class,
     ];
 
     /**
