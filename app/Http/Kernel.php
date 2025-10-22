45f4c4ebf805cbef75ed4e9eec160189794c3f44
Dan Nita <dan.nita@hdruk.ac.uk>
2025-10-22 14:37:20 +0100

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,8 +23,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Illuminate\Session\Middleware\StartSession::class,
-        // \App\Http\Middleware\TerminateRequest::class,
-        // \App\Http\Middleware\LogRequestResponse::class,
+        \App\Http\Middleware\TerminateRequest::class,
+        \App\Http\Middleware\LogRequestResponse::class,
     ];
 
     /**
