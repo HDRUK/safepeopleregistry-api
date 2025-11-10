a6227cc9075786f7c4c50cfefa9f7e53e6d2f526
Dan Nita <dan.nita@hdruk.ac.uk>
2025-11-10 16:17:45 +0000

    update

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -21,9 +21,9 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
         // Horizon::routeMailNotificationsTo('example@example.com');
         // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
 
-        Horizon::auth(function ($request) {
-            return Gate::check('viewHorizon', $request->user());
-        });
+        // Horizon::auth(function ($request) {
+        //     return Gate::check('viewHorizon', $request->user());
+        // });
     }
 
     /**
