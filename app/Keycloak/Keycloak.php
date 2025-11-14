ea453b9f676058d4844baf00c88273b745a51015
Dan Nita <dan.nita@hdruk.ac.uk>
2025-11-14 14:35:22 +0000

    update the email in keycloak when is updated in profile

--- app/Keycloak/Keycloak.php
+++ app/Keycloak/Keycloak.php
@@ -426,4 +426,55 @@ class Keycloak
             unset($payload);
         }
     }
+
+    public static function updateUserEmail(string $token, string $keycloakId, string $email): bool
+    {
+        $url = env('KEYCLOAK_BASE_URL').'/admin/realms/'.env('KEYCLOAK_REALM').'/users/' . $keycloakId;
+
+        try {
+            $response = Http::withToken($token)
+                ->withHeaders([
+                    'Content-Type' => 'application/json',
+                ])->put(
+                    $url,
+                    [
+                        'email' => $email,
+                        'emailVerified' => false,
+                    ],
+                );
+
+            if (!$response->successful()) {
+                throw new Exception('Failed to update email: ' . $email);
+            }
+
+            return true;
+        } catch (Exception $e) {
+            throw new Exception($e);
+        }
+    }
+
+    public static function sendVerifyEmail(string $token, string $keycloakId): bool
+    {
+        $url = env('KEYCLOAK_BASE_URL').'/admin/realms/'.env('KEYCLOAK_REALM').'/users/' . $keycloakId . '/execute-actions-email';
+
+        try {
+            $response = Http::withToken($token)
+                ->withHeaders([
+                    'Content-Type' => 'application/json',
+                ])->put(
+                    $url,
+                    [
+                        'VERIFY_EMAIL',
+                    ],
+                );
+
+            if (!$response->successful()) {
+                throw new Exception('Failed to send verification email');
+            }
+
+            return true;
+        } catch (Exception $e) {
+            throw new Exception($e);
+        }
+    }
 }
