2b18d12cdee8645bfd7e92c40ef5c3bedfc56f0f
peter-hammans <164904644+peter-hammans@users.noreply.github.com>
2025-12-01 15:34:36 +0000

    feat(REGISTRY-2237): filter no affilliations (#551)

--- app/Http/Controllers/Api/V1/PendingInviteController.php
+++ app/Http/Controllers/Api/V1/PendingInviteController.php
@@ -63,6 +63,15 @@ class PendingInviteController extends Controller
             ->with([
                 'user:id,name,email,user_group,unclaimed'
             ])
+            ->whereHas('user', function ($q1) {
+                $q1->where(function ($q2) {
+                    $q2->where([
+                        'user_group' => User::GROUP_USERS,
+                    ])
+                    ->whereHas('registry.affiliations');
+                })
+                ->orWhere('user_group', '!=', User::GROUP_USERS);
+            })
             ->paginate((int)$this->getSystemConfig('PER_PAGE'));
 
         return $this->OKResponse($pendingInvites);
