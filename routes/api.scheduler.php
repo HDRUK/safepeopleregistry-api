9f06d31b5fac80f2b68bf5db075f514aa5b9cbb6
Dan Nita <dan.nita@hdruk.ac.uk>
2025-12-02 11:11:08 +0000

    update

--- routes/api.scheduler.php
+++ routes/api.scheduler.php
@@ -9,3 +9,12 @@ Route::get('/checking_certificates', function (Request $request) {
         'message' => 'ok',
     ], 200);
 });
+
+
+Route::get('/cheking_security_compliance', function (Request $request) {
+    Artisan::call('app:cheking-security-compliance');
+
+    return response()->json([
+        'message' => 'ok',
+    ], 200);
+});
