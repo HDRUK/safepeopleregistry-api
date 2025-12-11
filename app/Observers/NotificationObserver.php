38bff0d06e6ea0d30c0bd4441527d42f1d34da60
Dan Nita <dan.nita@hdruk.ac.uk>
2025-12-11 13:33:04 +0000

    update

--- app/Observers/NotificationObserver.php
+++ app/Observers/NotificationObserver.php
@@ -10,20 +10,10 @@ class NotificationObserver
     {
         if (isset($notification->data['causer_id'])) {
             $notification->causer_id = $notification->data['causer_id'];
-
-            // Optionally remove it from the data array to avoid duplication
-            $data = $notification->data;
-            // disabled temp
-            // unset($data['causer_id']);
-            $notification->data = $data;
         }
 
-        // You can do the same for other custom fields
         if (isset($notification->data['causer_action'])) {
             $notification->causer_action = $notification->data['causer_action'];
-            // disabled temp
-            // unset($notification->data['causer_action']);
-            $notification->data = $notification->data;
         }
     }
 }
