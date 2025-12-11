1e12c83096858fcd7edcf776628f0a9282700b16
Dan Nita <dan.nita@hdruk.ac.uk>
2025-12-11 13:30:16 +0000

    update

--- app/Http/Controllers/Api/V1/TestController.php
+++ app/Http/Controllers/Api/V1/TestController.php
@@ -11,29 +11,8 @@ class TestController extends Controller
 {
     public function test(Request $request)
     {
-        $projects = Project::query()
-            ->where('id', 5)
-            ->with([
-                'projectHasSponsorships.sponsor',
-                'projectHasSponsorships.custodianHasProjectHasSponsorship.modelState.state',
-            ])
-            ->get();
-
-        //    if (Feature::active('sponsorship')) {
-        //         return response()->json([
-        //             'message' => 'active',
-        //         ], 200);
-        //     }
-
-        //     if (Feature::inactive('sponsorship')) {
-        //         return response()->json([
-        //             'message' => 'inactive',
-        //         ], 200);
-        //     }
-
         return response()->json([
             'message' => 'success',
-            'data' => $projects,
         ], 200);
     }
 }
