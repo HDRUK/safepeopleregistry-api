7620de9c8d9828086cbbe172dbe3e451ff7a6741
Dan Nita <dan.nita@hdruk.ac.uk>
2025-12-15 11:57:52 +0000

    update

--- app/Http/Controllers/Api/V1/TestController.php
+++ app/Http/Controllers/Api/V1/TestController.php
@@ -12,17 +12,8 @@ class TestController extends Controller
 {
     public function test(Request $request)
     {
-        $custodianIds = ProjectHasCustodian::where('project_id', 1)->get()->pluck('custodian_id')->toArray();
-
-        $userCustodians = User::whereIn(
-            'custodian_user_id',
-            CustodianUser::where('custodian_id', $custodianIds)->pluck('id')
-        )->get();
-
         return response()->json([
             'message' => 'success',
-            'custodianIds' => $custodianIds,
-            'userCustodians' => $userCustodians,
         ], 200);
     }
 }
