72fb43f576256b16040d5f6820f12233c8bc9acf
peter-hammans <164904644+peter-hammans@users.noreply.github.com>
2026-01-12 14:24:02 +0000

    feat(REGISTRY-2244): modify org history endpoint (#603)

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -24,6 +24,7 @@ use App\Models\UserHasDepartments;
 use App\Traits\TracksModelChanges;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Users\GetUser;
+use App\Http\Requests\Users\UpdateUserKeycloak;
 use Illuminate\Support\Facades\Gate;
 use App\Http\Requests\Users\CreateUser;
 use App\Http\Requests\Users\DeleteUser;
@@ -868,16 +869,26 @@ class UserController extends Controller
     }
 
     // Hide from swagger docs
-    public function updateKeycloakUserEmailById(GetUser $request, int $id)
+    public function updateKeycloakUserEmailById(UpdateUserKeycloak $request, int $id)
     {
-        $user = User::where('id', $id)->first();
+        try {
+            $input = $request->all();
+            $email = $input['email'];
+            $keycloakToken = $this->getAuthToken();
+            $user = User::where('id', $id)->first();;
 
-        $keycloakToken = $this->getAuthToken();
+            Keycloak::updateUserEmail($keycloakToken, $user->keycloak_id, $email);
 
-        Keycloak::updateUserEmail($keycloakToken, $user->keycloak_id, $user->email);
-        Keycloak::sendVerifyEmail($keycloakToken, $user->keycloak_id);
+            $user->update([
+               'email' => $email
+            ]);
 
-        return $this->OKResponse('Verification email has been sent.');
+            Keycloak::sendVerifyEmail($keycloakToken, $user->keycloak_id);
+
+            return $this->OKResponse('Verification email has been sent.');
+        } catch (Exception $e) {
+            return $this->ErrorResponse($e->getMessage());
+        }
     }
 
     // Hide from swagger docs
