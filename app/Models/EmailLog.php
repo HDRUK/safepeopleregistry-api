2e49f483affcb72306878c2c666b9f2209468939
Dan Nita <dan.nita@hdruk.ac.uk>
2026-01-26 09:25:09 +0000

    feat(REGISTRY-2481): Create table for sent email history

---
+++ app/Models/EmailLog.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class EmailLog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'to',
+        'subject',
+        'type',
+        'data',
+        'template',
+        'job_uuid',
+        'job_status',
+        'message_id',
+        'message_status',
+        'message_response',
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+        'job_status' => 'integer',
+    ];
+}
---
+++ database/migrations/2026_01_26_090923_create_email_logs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('email_logs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('to')->nullable()->index();
+            $table->string('subject')->nullable()->index();
+            $table->string('type')->nullable();
+            $table->json('data')->nullable();
+            $table->string('template')->nullable();
+
+            $table->string('job_uuid')->nullable();
+            $table->tinyInteger('job_status')->default(1);
+
+            $table->string('message_id')->nullable();
+            $table->string('message_status')->nullable();
+            $table->text('message_response')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('email_logs');
+    }
+};
