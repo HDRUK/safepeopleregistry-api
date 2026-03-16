11f8054d1694b85e7c83b2dca7a7848794fc1a15
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2026-03-16 09:33:33 +0000

    reset

--- app/Traits/OrganisationsProjectUtils.php
+++ app/Traits/OrganisationsProjectUtils.php
@@ -13,22 +13,9 @@ trait OrganisationsProjectUtils
             ->whereHas('projectOrganisation', function ($query) use ($org) {
                 $query->where('organisation_id', $org->id);
             })->get();
-            \Log::info('<<<<<<<$custodianHasProjectOrganisations');
 
         foreach ($custodianHasProjectOrganisations as $custodianHasProjectOrganisation) {
-                $currentState = $custodianHasProjectOrganisation->getState();
-
-                $canIBeACustard = $custodianHasProjectOrganisation->canTransitionTo($state);
-                \Log::info('<<<<<<<$sometimesIEatSundayRoasts '.$canIBeACustard);
-                \Log::info('<<<<<<<$currentState '.$currentState);
-                \Log::info('<<<<<<<$state '.$state);
-
-
-
-            if ($custodianHasProjectOrganisation->getState() !== $state) {
-                $custodianHasProjectOrganisation->setState($state);
-            }
-            
+            $custodianHasProjectOrganisation->setState($state);
         }  
     }
 }
