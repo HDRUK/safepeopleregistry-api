7d851e2ad6150c3e3e569603048dca842ef55eba
Sam Cox <spco@users.noreply.github.com>
2026-06-26 10:43:57 +0100

    fix(REGISTRY-0000): Fix naming of facade to meet psr-4 autoloading standard (#752)

--- app/RulesEngineManagementController/RulesEngineManagementControllerFacade.php
+++ app/RulesEngineManagementController/RulesEngineManagementControllerFacade.php
@@ -4,7 +4,7 @@ namespace App\RulesEngineManagementController;
 
 use Illuminate\Support\Facades\Facade;
 
-class RulesEngineManagementController extends Facade
+class RulesEngineManagementControllerFacade extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
