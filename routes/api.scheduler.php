c2a8ca46d757efcd331ea4d6ac165fec7435966b
Sam Cox <sam.cox@hdruk.ac.uk>
2026-06-29 09:57:06 +0100

    Fix typos of 'checking' too

--- routes/api.scheduler.php
+++ routes/api.scheduler.php
@@ -10,15 +10,15 @@ Route::get('/checking_trainings', function (Request $request) {
     ], 200);
 });
 
-Route::get('/cheking_security_compliance', function (Request $request) {
-    Artisan::call('app:cheking-security-compliance');
+Route::get('/checking_security_compliance', function (Request $request) {
+    Artisan::call('app:checking-security-compliance');
 
     return response()->json([
         'message' => 'ok',
     ], 200);
 });
 
-Route::get('/cheking_end_projects', function (Request $request) {
+Route::get('/checking_end_projects', function (Request $request) {
     Artisan::call('app:checking-projects');
 
     return response()->json([
@@ -26,15 +26,15 @@ Route::get('/cheking_end_projects', function (Request $request) {
     ], 200);
 });
 
-Route::get('/cheking_user_automated_flags', function (Request $request) {
-    Artisan::call('aapp:checking-user-automated-flags');
+Route::get('/checking_user_automated_flags', function (Request $request) {
+    Artisan::call('app:checking-user-automated-flags');
 
     return response()->json([
         'message' => 'ok',
     ], 200);
 });
 
-Route::get('/cheking_organisation_automated_flags', function (Request $request) {
+Route::get('/checking_organisation_automated_flags', function (Request $request) {
     Artisan::call('app:checking-organisation-automated-flags');
 
     return response()->json([
