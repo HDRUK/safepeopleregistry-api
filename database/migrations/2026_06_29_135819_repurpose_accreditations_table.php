d551bbb3caa2734e8e2bd5456e572e09d888103a
Sam Cox <spco@users.noreply.github.com>
2026-07-02 11:32:43 +0100

    fix(REGISTRY-000): Fix linting issues (#759)

--- database/migrations/2026_06_29_135819_repurpose_accreditations_table.php
+++ database/migrations/2026_06_29_135819_repurpose_accreditations_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        
+
         Schema::table('accreditations', function (Blueprint $table) {
             $table->string('associated_organisation_name')->nullable()->after('id');
             $table->string('id_string')->after('associated_organisation_name');
