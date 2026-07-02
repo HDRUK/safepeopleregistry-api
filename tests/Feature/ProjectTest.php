e8ff205b8271e53fabce5d393357b725bced6cdb
Sam Cox <sam.cox@hdruk.ac.uk>
2026-07-02 09:50:09 +0100

    Sort yourself out

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -736,7 +736,7 @@ class ProjectTest extends TestCase
 
     public function test_the_application_can_get_project_users(): void
     {
-        $response = $this->actingAs(Custodian::first()->id)
+        $response = $this->actingAs(Custodian::first())
             ->json(
                 'GET',
                 self::TEST_URL . "/2/all_users"
@@ -746,7 +746,7 @@ class ProjectTest extends TestCase
 
     public function test_the_application_can_get_project_users_filter_in(): void
     {
-        $response = $this->actingAs(Custodian::first()->id)
+        $response = $this->actingAs(Custodian::first())
             ->json(
                 'GET',
                 self::TEST_URL . "/2/all_users?user_project_filter=in"
@@ -760,7 +760,7 @@ class ProjectTest extends TestCase
 
     public function test_the_application_can_assign_claimed_users_to_project(): void
     {
-        $responseUsersInProjectBefore = $this->actingAs(Custodian::first()->id)
+        $responseUsersInProjectBefore = $this->actingAs(Custodian::first())
             ->json(
                 'GET',
                 self::TEST_URL . "/2/all_users?user_project_filter=in"
