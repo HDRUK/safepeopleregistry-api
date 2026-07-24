8691512cf2571b62dfb7ffbae944715e6ffcfa8f
Sam Cox <sam.cox@hdruk.ac.uk>
2026-07-24 12:10:28 +0100

    Fix state test and remove an unused var

--- tests/Unit/StateWorkflowTest.php
+++ tests/Unit/StateWorkflowTest.php
@@ -191,14 +191,14 @@ class StateWorkflowTest extends TestCase
         $affiliation = Affiliation::where('id', 1)->first();
         var_dump('Pre getstate', $affiliation->getState());
 
-        $this->assertTrue($affiliation->getState() === State::STATE_AFFILIATION_PENDING);
-        $verficationCode = $affiliation->verification_code;
+        $this->assertTrue($affiliation->getState() === State::STATE_AFFILIATION_EMAIL_VERIFY);
+        $verificationCode = $affiliation->verification_code;
         $organisationId = $affiliation->organisation_id;
 
         $response = $this->actingAsKeycloakUser($this->user)
             ->json(
                 'PUT',
-                "/api/v1/affiliations/verify_email/{$verficationCode}",
+                "/api/v1/affiliations/verify_email/{$verificationCode}",
                 []
             );
 
